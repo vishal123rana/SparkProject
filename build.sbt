9c6261d31478adc63824cb2f9f3931be951f5e0a
vishal rana <vishalrana03021998@gmail.com>
2021-01-03 23:21:10 +0530

    kafka + spark

--- build.sbt
+++ build.sbt
@@ -6,4 +6,7 @@ scalaVersion := "2.11.12"
 
 // https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
 libraryDependencies += "org.apache.kafka" % "kafka-clients" % "2.6.0"
+// https://mvnrepository.com/artifact/kafka-utils/kafka-utils
+
+
 
--- src/main/scala/StructuredStreaming/kafkaPlusSpark.scala
+++ src/main/scala/StructuredStreaming/kafkaPlusSpark.scala
@@ -1,7 +1,25 @@
 package StructuredStreaming
 
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+import scala.collection.mutable
+
 object kafkaPlusSpark {
-    def main(args:Array[String]):Unit={
-        
+    def main(args:Array[String]):Unit= {
+        val kafkaParams = Map[String,Object](
+            "bootstrap.servers"->"localhost:9092",
+            "key.deserializer"->classOf[StringDeserializer],
+            "value.deserializer"->classOf[StringDeserializer],
+            "auto.offset.reset" -> "latest",
+            "enable.auto.commit" -> (false:java.lang.Boolean)
+        )
+        //val conf = new SparkConf().setAppName("Streaming").setMaster("local");
+        val spark = SparkSession.builder().master("local").appName("Application").getOrCreate()
+       // val streaming = spark.readStream.format("kafka").option("kafka.bootstrap.servers","localhost:9092").option("subscribe","topic")
+        val topics = Array("test")
+
     }
 }
